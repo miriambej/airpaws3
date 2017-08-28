2f4793ecfc7d60b46f28f016e1ad3a427425c112
Miriam Espinosa Jorrin <miriam@192-168-1-2.tpgi.com.au>
2017-08-28 23:01:30 +1000

    add gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def avatar_url(user) #we get the user.email by the user input parameter
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase #we use Digest::MD5::hexdigest method from rails to put the gravatar id
+    "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150" # we return thre string and the syntax as a link.
+
+  end
 end
