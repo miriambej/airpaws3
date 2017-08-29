589b6ab0b7c1d888620e6c223e8e1dc9e0ed1ad5
miriambej <justme0112@hotmail.com>
2017-08-29 21:02:14 +1000

    add style and back to gmail auth

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def avatar_url(user) #we get the user.email by the user input parameter
 
     if user.image
-      "http://graph.facebook.com/#{user.id}/picture?type=large" #display facebook image
+      "http://graph.facebook.com/#{user.uid}/picture?type=large" #display facebook image
     else
       gravatar_id = Digest::MD5::hexdigest(user.email).downcase #we use Digest::MD5::hexdigest method from rails to put the gravatar id
       "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150" # we return thre string and the syntax as a link.
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,20 +56,11 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.mailgun.org',
+    address: 'smtp.gmail.com',
     port: 587,
-    domain: 'sandboxebbee629a05a4384a70c0bf47c7a3c76.mailgun.org',
+    enable_starttls_auto: true,
     authentication: 'plain',
-    user_name: 'postmaster@sandboxebbee629a05a4384a70c0bf47c7a3c76.mailgun.org',
-    password: '3baa1d6b1ab5b3cbf5ad8a9c0c2318c4'
+    user_name: 'heroku.miriam23@gmail.com',
+    password: '*****'
   }
-
-  # config.action_mailer.smtp_settings = {
-  #   address: 'smtp.gmail.com',
-  #   port: 587,
-  #   enable_starttls_auto: true,
-  #   authentication: 'plain',
-  #   user_name: 'heroku.miriam23@gmail.com',
-  #   password: '*********'
-  # }
 end
