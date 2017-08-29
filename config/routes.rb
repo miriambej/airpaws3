a1128c0d12d9e7db3dce4925a02fa2f8f91a09f4
miriambej <justme0112@hotmail.com>
2017-08-29 22:51:31 +1000

    create user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
               path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'}, #this is to change the path ie., whenever they sign in will be /login if they signup it will be /registration
               controllers: {omniauth_callbacks: 'omniauth_callbacks'}
 
+  resources :users, only: [:show] #we want to create the path for showing user information
+
 end
