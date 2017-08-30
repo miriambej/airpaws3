7aff01079949299c26103fe3cadad321b8178626
miriambej <justme0112@hotmail.com>
2017-08-30 23:06:21 +1000

    create room index page

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,4 +4,13 @@ class Room < ApplicationRecord
 
   validates :home_type, presence: true
   validates :accommodate, presence: true
+
+  def cover_photo(size)  #we are using this method in index.html.erb
+    if self.photos.length > 0
+      self.photos[0].image.url(size)
+    else
+      "blank.jpg"
+    end
+  end
+
 end
