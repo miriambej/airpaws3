f955c309a9a0cc68295d0dd3bc601c0297916cd2
miriambej <justme0112@hotmail.com>
2017-08-31 16:04:05 +1000

    test

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,14 +2,12 @@ class PhotosController < ApplicationController
 
   def create
     @room = Room.find(params[:room_id])
-
-    if params[:image]
+    if params[:images]
       params[:images].each do |img|
         @room.photos.create(image: img)
       end
-
-      @photos = @room.photos
-      redirect_back(fallback_location: request.referer, notice: "Saved...")
     end
+    @photos = @room.photos
+    redirect_back(fallback_location: request.referer, notice: "Saved...")
   end
 end
