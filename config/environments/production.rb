3fce6e9f9b44b1dbfd3081320abaa5b0b203a024
miriambej <justme0112@hotmail.com>
2017-08-31 10:58:09 +1000

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'airpaws.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    enable_starttls_auto: true,
+    authentication: 'plain',
+    user_name: 'heroku.miriam23@gmail.com',
+    password: Rails.application.secrets.gm_1
+  }
 end
